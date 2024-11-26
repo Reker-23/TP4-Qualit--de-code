a79f46875a296a36be7d0779b592983e94970133
Adrien Hussenet <hussenea@3il.fr>
2024-11-26 14:23:31 +0100

    fractionnement de la méthode afficherNotesEtMoyenne en calculerMoyenne et afficherNotes

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,13 +1,19 @@
+/**
+ * Cette classe gère des notes d'élèves
+ */
+
 public class GestionnaireNotes {
 
     // Affiche note et moyennes
     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
-        System.out.println("Notes de " + nomEtudiant + ":");
-        for (int note : notes) {
-            System.out.println("- " + note);
-        }
 
+        afficherNotes(nomEtudiant, notes);
+        double moyenne = calculerMoyenne(notes);
+        System.out.println("Moyenne : " + moyenne);
+    }
+
+    //calul la moyenne d'un élève
+    public double calculerMoyenne(int[] note) {
         // Calcul de la somme des notes
         int somme = 0;
         for (int note : notes) {
@@ -16,6 +22,14 @@ public class GestionnaireNotes {
 
         // Affichage de la moyenne
         double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
+    }
+
+    //affiche les notes d'un étudiant
+    public void afficherNotes(String nomEtudiant, int[] notes) {
+        // Affichage des notes
+        System.out.println("Notes de " + nomEtudiant + ":");
+        for (int note : notes) {
+            System.out.println("- " + note);
+        }
     }
 }
