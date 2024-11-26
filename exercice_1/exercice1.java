bd0e80a7d9124cc3a91fc98152ebcb10e50e738e
Adrien Hussenet <hussenea@3il.fr>
2024-11-26 13:51:37 +0100

    Fractionnement de calculer facture

--- exercice_1/exercice1.java
+++ exercice_1/exercice1.java
@@ -4,6 +4,14 @@ public class FactureManager {
     public double calculerFacture(String typeProduit, int quantite, double prixUnitaire) {
         double total = quantite * prixUnitaire;
 
+        total = reductionCategorieItem(typeProduit,total);
+
+        total = reductionSurTotal(total);
+
+        return total;
+    }
+    //retourne le prix après la réduction selon le type de produit
+    public double reductionCategorieItem(String typeProduit, double total){
         // reduction selon la catégorie
         if (typeProduit.equals("Alimentaire")) {
             total -= total * 0.05; // Réduction de 5%
@@ -12,12 +20,16 @@ public class FactureManager {
         } else if (typeProduit.equals("Luxe")) {
             total -= total * 0.15; // Réduction de 15%
         }
+        return total;
+    }
 
+    //applique une réduction si le total est supérieur à 1000
+    public double reductionSurTotal(double total){
         // Reduction sur le total
         if (total > 1000) {
             total -= total * 0.05; // Réduction supplémentaire de 5% pour les gros montants
         }
-
         return total;
     }
+
 }
